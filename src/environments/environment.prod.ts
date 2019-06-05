8f6eb9a60b96e47b5b84da5094889a238f7febf5
Takumi Adachi <takumiadachi@gmail.com>
2019-06-05 08:10:15 -0700

    Add a production client id and redirect uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   name: "prod",
   twitch_username: "",
   twitch_oauth_pass: "",
-  twitch_clientId: "v46986rq8azaksvs007x1jtowesv4d",
+  twitch_clientId: "fin8yorw5alqnawk2wvzubyez7jkxe",
   twitch_access_token: "access_token",
   twitch_refresh_token: "refresh_token",
   twitch_client_secret: "client_secret",
-  twitch_redirect_uri: ""
+  twitch_redirect_uri: "https://elastic-borg-0e66e5.netlify.com/login"
 };
