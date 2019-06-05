8a404833ebfbde180ec0be8ae13b0fd32b24e343
Takumi Adachi <takumiadachi@gmail.com>
2019-06-05 09:35:36 -0700

    Try again adding _redirects to angular.json build.
    See: https://medium.com/@mgd4375/how-to-enable-angular-routing-in-a-netlify-deployment-with-the-angular-cli-e2eda69f1b5b

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,10 @@ export const environment = {
   twitch_username: "",
   twitch_oauth_pass: "",
   twitch_clientId: "fin8yorw5alqnawk2wvzubyez7jkxe",
+  // twitch_clientId: "v46986rq8azaksvs007x1jtowesv4d",
   twitch_access_token: "access_token",
   twitch_refresh_token: "refresh_token",
   twitch_client_secret: "client_secret",
   twitch_redirect_uri: "https://elastic-borg-0e66e5.netlify.com/login"
+  // twitch_redirect_uri: "http://localhost:4200/login"
 };
